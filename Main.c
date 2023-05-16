2ce8ab374c69cae49028cb0b22a113c2f6f1bad0
Eduardo <eduardoaguliari0608@gmail.com>
2023-05-16 14:35:18 -0300

    Prompting the user to set variables

--- Main.c
+++ Main.c
@@ -6,7 +6,7 @@
 int main()
 {
     int people;
-    float distance, fuel_consumption = 6.5, fuel_price = 3.8, total_fuel, total_cost, total_cost_person;
+    float distance, fuel_consumption, fuel_price, total_fuel, total_cost, total_cost_person;
     char option = 'Y';
 
     do
@@ -19,14 +19,20 @@ int main()
         printf("Enter with the amount of people: ");
         scanf("%d", &people);
 
+        printf("Enter with the fuel consumption (in km/l): ");
+        scanf("%f", &fuel_consumption);
+
+        printf("Enter with the fuel price (per liter): $ ");
+        scanf("%f", &fuel_price);
+
         total_fuel = distance / fuel_consumption;
         total_cost = total_fuel * fuel_price;
         total_cost_person = total_cost / people;
 
         printf("---------------------------------\n");
         printf("Total fuel consumption: %.2f L\n", total_fuel);
-        printf("Total cost: R$ %.2f\n", total_cost);
-        printf("Total for each person: R$ %.2f\n", total_cost_person);
+        printf("Total cost: $ %.2f\n", total_cost);
+        printf("Total for each person: $ %.2f\n", total_cost_person);
         printf("---------------------------------\n");
 
         printf("To redo the calculation, type Y, otherwise press any key: ");
